6f5c4dd5b4787cfddbe09305eb2de2227fae7c4b
Marvin Sojo <119646998+marvinsojo@users.noreply.github.com>
2024-01-31 00:54:48 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,34 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon", "puedes", "internet", "telecom", "airbus"];
+  let ext = [".com", ".net", ".us", ".io", ".es"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    pronoun.forEach(pronombre => {
+      adj.forEach(adjetivo => {
+        noun.forEach(sustantivo => {
+          ext.forEach(extension => {
+            let sustantivoHack = sustantivo.slice((extension.length - 1) * -1);
+            let extensionHack = extension.slice((extension.length - 1) * -1);
+            let sustantivoHackSinExt = sustantivo.slice(
+              0,
+              sustantivo.length - (extension.length - 1)
+            );
+
+            if (sustantivoHack == extensionHack) {
+              let domainHack;
+              domainHack =
+                pronombre + adjetivo + sustantivoHackSinExt + extension;
+              console.log(domainHack);
+            } else {
+              console.log(pronombre + adjetivo + sustantivo + extension);
+            }
+          });
+        });
+      });
+    });
+  }
 };
